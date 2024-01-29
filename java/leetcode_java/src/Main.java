9b486e31f62fb416cf823b480a6858cd8ae9a86a
rich <rich.leblanc1@proton.me>
2024-01-28 23:59:22 -0500

    Improved output for 1480. Running Sum of 1D Array in java

--- java/leetcode_java/src/Main.java
+++ java/leetcode_java/src/Main.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Running leetcode problems");
+        System.out.println("Running leetcode problems");
 
         // 1480. Running Sum of 1D Array
+        System.out.println("1480. Running Sum of 1D Array");
         Vector<Integer> numbers = new Vector<>(List.of(3, 1, 2, 10, 1));
 
         RunningSumOf1DArray1480 solution = new RunningSumOf1DArray1480();
         solution.calculateRunningSum(numbers);
 
         // 1672. Richest Customer Wealth
-
+        System.out.println("1672. Richest Customer Wealth");
+        //rcw = calculate_richest_customer_wealth(accounts);
     }
 }
--- java/leetcode_java/src/RichestCustomerWealth1672.java
+++ java/leetcode_java/src/RichestCustomerWealth1672.java
@@ -2,25 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
-public class RunningSumOf1DArray1480 {
+public class RichestCustomerWealth1672 {
     public void calculateRunningSum(Vector<Integer> numbers) {
-        //System.out.print("1480. Running Sum of 1D Array");
+        System.out.print("1480. Running Sum of 1D Array");
 
         //int[] numbers = {3, 1, 2, 10, 1};
         int running_sum = 0;
         List<Integer> result_list = new ArrayList<>();
 
         // print the numbers array
-        System.out.print("input: ");
-        System.out.println(numbers);
-
         for (int num : numbers) {
-            //System.out.printf("%d ", num);
+            System.out.println(num);
             running_sum += num;
             result_list.add(running_sum);
         }
 
-        System.out.print("output: ");
         System.out.println(result_list);
     }
 }
