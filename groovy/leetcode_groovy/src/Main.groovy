d9cce1f213acd10a085984be22792bc42fb075a7
rich <rich.leblanc1@proton.me>
2024-01-30 17:48:37 -0500

    Finished 1672. Richest Customer Wealth in groovy

--- groovy/leetcode_groovy/src/Main.groovy
+++ groovy/leetcode_groovy/src/Main.groovy
@@ -1,12 +1,18 @@
 import RunningSumOf1DArray1480
+import RichestCustomerWealth1672
 
 static void main(String[] args) {
-    // 1480. Running Sum of 1D Array
     println "1480. Running Sum of 1D Array"
     def numbers = [1, 2, 3, 4, 5]
-
     running_sums = new RunningSumOf1DArray1480()
     def result_list = running_sums.calculateRunningSum(numbers)
     println "Result List: $result_list"
+
+    println "1672. Richest Customer Wealth"
+    def accounts = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    highest_balance = RichestCustomerWealth1672.calculateRichestCustomerWealth(accounts)
+    println "highest_balance: $highest_balance"
+
+
 }
 
---
+++ groovy/leetcode_groovy/src/RichestCustomerWealth1672.groovy
@@ -0,0 +1,15 @@
+// RichestCustomerWealth1672.groovy
+
+class RichestCustomerWealth1672 {
+    static int calculateRichestCustomerWealth(List<List<Integer>> accounts) {
+        int highestWealth = 0
+
+        // Iterate over each customer's account
+        for (List<Integer> account : accounts) {
+            int wealth = account.sum() // Calculate the wealth of the current customer
+            highestWealth = Math.max(highestWealth, wealth) // Update highest wealth if necessary
+        }
+
+        return highestWealth
+    }
+}
