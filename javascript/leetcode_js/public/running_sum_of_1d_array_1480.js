6f58fd4487db8dce666c8f21fd61ec9f6a35dbed
rich <rich.leblanc1@proton.me>
2024-01-29 22:47:26 -0500

    Finished 1672. Richest Customer Wealth in javascript

--- javascript/leetcode_js/public/running_sum_of_1d_array_1480.js
+++ javascript/leetcode_js/public/running_sum_of_1d_array_1480.js
@@ -1,19 +1,28 @@
 // running_sum_of_1d_array_1480.js
+// 1480. Running Sum of 1D Array
 // print input list
 // sum numbers
-// append sums to result_list
-let numbers = [3, 1, 2, 10, 1]
-let running_sum = 0
-let result_list = []
+// add sums to result_list
 
-console.log("input_list: ", numbers)
-console.log("running_sum: ", running_sum)
+function process_numbers(numbers) {
+    let running_sum = 0
+    let result_list = []
 
-//
-for (let num of numbers) {
-    console.log("running_sum: ", running_sum);
-    running_sum += num;
-    result_list.push(running_sum);
+    console.log("input_list: ", numbers)
+    console.log("running_sum: ", running_sum)
+
+    // push sums to result list
+    for (let num of numbers) {
+        running_sum += num;
+        result_list.push(running_sum);
+        console.log("running_sum: ", running_sum);
+    }
+
+    return result_list
 }
+
+let numbers = [3, 1, 2, 10, 1]
+
+result_list = process_numbers(numbers)
 
 console.log("result_list: ", result_list)
